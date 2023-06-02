fa556a9a147eb21dc463f2f67c0ccd7f2a61e823
jpgb-hub <jpgbonnet@gmail.com>
2023-06-02 12:58:01 -0400

    iconos+musica

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,65 @@
 import React from "react";
+import { useRef } from "react";
+import {BsFillRewindFill} from 'react-icons/bs'; //importa los iconos//
+import {BsFillPlayFill} from 'react-icons/bs';
+import {BsFillFastForwardFill} from 'react-icons/bs';
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+{/*import rigoImage from "../../img/rigo-baby.jpg";*/ }
+const songURL = "https://assets.breatheco.de/apis/sound/"
+let song_1 = songURL + "files/mario/songs/castle.mp3";
+let song_2= songURL + "files/mario/songs/hurry-starman.mp3";
+let song_3 = songURL + "files/mario/songs/overworld.mp3";
+
 
 //create your first component
 const Home = () => {
 	return (
+		<>
+		<div className="container">
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">Mario´s Playlist</h1>
+			<ul className="list-group">
+				<li className="list-group-item" >1. An item</li>
+				<li className="list-group-item">2. second item</li>
+				<li className="list-group-item">3. third item</li>
+			</ul>
+
+					
+			
+			<div>
+				<div className="alert alert-light" role="alert">
+					<button type="button" className="btn" >
+						<BsFillRewindFill/>
+					</button>
+					<button type="button" className="btn">
+						<BsFillPlayFill/>
+					</button>
+					<button type="button" className="btn">
+						<BsFillFastForwardFill/>
+					</button>
+				</div>
+			</div>
+
+
+				
+			
+
+			
+         
+		 
+
+
+		
+
 		</div>
+		</div>
+		</>
+
+	
 	);
+
+
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from 'prop-types'
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+{/*import{FontAwesomeIcon} from '@fortawesome/react-fontawesome'; {/*importa la libreria font awesome*/}
+{/*import{faClock} from '@fortawesome/free-solid-svg-icons';import { func } from "prop-types";{/*importa el icono font awesome*/}
 
 //import your own components
 import Home from "./component/home.jsx";
